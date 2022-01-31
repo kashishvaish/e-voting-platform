1f8d514ae53afa70bd6b72358c9e55a9ebf9ee36
kashishvaish <kashish.vaish.cs.2018@miet.ac.in>
2022-01-31 23:01:48 +0530

    Configured Settings

--- eVoting/settings.py
+++ eVoting/settings.py
@@ -18,8 +18,8 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(SECRET_KEY = str,)
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
