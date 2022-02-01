d44c94483e7397be0bb2fe2f4a00eedf87f77914
kashishvaish <kashish.vaish.cs.2018@miet.ac.in>
2022-02-01 16:06:23 +0530

    Configured Settings

--- eVoting/settings.py
+++ eVoting/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ['DEBUG']
 
-ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']]
+ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME'], '127.0.0.1']
 
 
 # Application definition
